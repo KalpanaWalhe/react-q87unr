8dcf4fc87eeafde7b48fb41bc1bf28caed1394ea
KalpanaWalhe <68656831+KalpanaWalhe@users.noreply.github.com>
2022-09-08 14:58:50 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 //import './style.css';
-import { Version } from './ReactRev/Version';
+import { Welcome } from './ReactRev/Welcome';
 //mport styles from './myAppStyles.module.css';
 export default function App() {
   return (
     <div>
-      <Version />
+      <Welcome />
 
       {/* {<h1 className={styles.success}>Success</h1>} */}
     </div>
---
+++ src/components/Welcome.js
@@ -0,0 +1,6 @@
+import { Component } from 'react';
+export class Welcome extends Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
